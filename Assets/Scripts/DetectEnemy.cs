63ebbe517660805b49fc5d6fa23ad9823bab785d
Reece9 <131805686+Reece9@users.noreply.github.com>
2024-03-03 03:48:38 +0000

    Player jump on detection
    Player now slightly jumps up if an enemy has been detected.
    Added code to store original position, move up slightly when enemy has been detected and return to original position when enemy has left detection zone

--- Assets/Scripts/DetectEnemy.cs
+++ Assets/Scripts/DetectEnemy.cs
@@ -6,14 +6,18 @@ public class DetectEnemy : MonoBehaviour
 {
     public Material redMaterial; // We can add this in the inspector. Red 
     public Material originalMaterial; // Change in inspector to Green
+    public float upwardMovement = 0.5f; //adjust how high in inspector
+    private Vector3 originalPosition;
 
     // Start is called before the first frame update
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Enemy")
         {
+            originalPosition = transform.position;
             // Enemy Will be Detected 
             GetComponent<Renderer>().material = redMaterial;
+            transform.position = new Vector3(transform.position.x, transform.position.y + upwardMovement, transform.position.z);
             
         }
     }
@@ -25,6 +29,7 @@ public class DetectEnemy : MonoBehaviour
         {
             // Enemy has left the detection zone
             GetComponent<Renderer>().material = originalMaterial;
+            transform.position = originalPosition;
         }
     }
 }
